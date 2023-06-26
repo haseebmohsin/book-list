069f0a754684a6e4e9ed34fecedf7ea3773a4c2f
haseeb11634 <haseeb11634@gmail.com>
2023-06-27 00:12:22 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
+const BookListScreen = () => {
+  const [books, setBooks] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const baseUrl = 'http://localhost:5000/api';
+
+    try {
+      const response = await axios.get(`${baseUrl}/books`);
+
+      console.log(response);
+
+      setBooks(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleRefresh = async () => {
+    setRefreshing(true);
+    await fetchData();
+    setRefreshing(false);
+  };
+
+  const renderBookCard = ({ item }) => {
+    return (
+      <View>
+        <Image source={{ uri: item.coverImage }} style={{ width: 100, height: 150 }} />
+        <Text>{item.title}</Text>
+        <Text>Discount rate: {item.discountRate}%</Text>
+        <Text>Price: {item.price}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <FlatList
+        data={books}
+        renderItem={renderBookCard}
+        keyExtractor={(item) => item.id}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}
+        onEndReachedThreshold={0.8}
+        onEndReached={handleRefresh}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default BookListScreen;
